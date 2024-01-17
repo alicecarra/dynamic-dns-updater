9ad69b1318891bcc277ddfe3b2c3cf5fb1f2b8ef
Alice Carra <aalicecarra@gmail.com>
2024-01-16 22:28:53 -0300

    getting ip from cloudflare api

--- cmd/dynamic-dns-updater/main.go
+++ cmd/dynamic-dns-updater/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"time"
@@ -55,19 +54,19 @@ func main() {
 
     OUTER:
     for {
-        dns_ip, err := net.LookupIP(recordname)
-        if err != nil {
-            log.Fatal(err)
-        }
+
+        dnsrecords := getdns(cloudflareconfigs)
+
+        fmt.Printf("%+v\n", dnsrecords)
 
         actual_ip, err := getip()
         if err != nil {
             log.Fatal(err)
         }
 
-        for _, ip := range dns_ip {
+        for _, record := range dnsrecords {
     
-            if ip.String() == actual_ip { 
+            if record.Content == actual_ip { 
                 log.Println("IP not changed")
 
                 time.Sleep(300 * time.Second)
@@ -142,6 +141,55 @@ func getip() (string, error) {
         return "", err
     }
     return ip.Query, nil
+}
+
+
+type DNSRecord struct {
+    Id string
+    Zone_id string
+    Zone_name string
+    Name string
+    Dns_type string
+    Content string
+    Proxiable bool
+    Proxied bool
+    Comment string
+    ModifiedOn string
+}
+
+type DnsRecordsResponse struct {
+    Result []DNSRecord
+}
+
+func getdns(cloudflareconfigs CloudflareConfigs) []DNSRecord {
+    apiurl := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", cloudflareconfigs.ZoneIdentifier)
+
+    request, err := http.NewRequest(
+        http.MethodGet,
+        apiurl,
+        nil,
+    )
+    if err != nil {
+        log.Fatalf("error creating HTTP request: %v", err)
+    }
+
+    request.Header.Set("Content-Type", "application/json")
+    request.Header.Set("X-Auth-Email", cloudflareconfigs.AuthEmail)
+    request.Header.Set("X-Auth-Key", cloudflareconfigs.AuthKey)
+
+
+    response, err := http.DefaultClient.Do(request)
+    if err != nil {
+        log.Fatalf("error sending HTTP request: %v", err)
+    }
+    
+    var dnsrecords DnsRecordsResponse
+	d := json.NewDecoder(response.Body)
+	if err := d.Decode(&dnsrecords); err != nil {
+		log.Fatalf("error deserializing dns data: %v", err)
+	}
+
+    return dnsrecords.Result
 }
 
 func updatedns(dnsconfig DNSConfig, cloudflareconfigs CloudflareConfigs) error {
